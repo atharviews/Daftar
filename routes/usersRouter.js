1c12e6c6ee4d364349f1f8e03c66b4b8cf3029bd
atharviews <Enter>
2024-09-11 22:17:59 +0530

    initial commit

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const userModel = require('../models/user-model');
 const router = express.Router();
 
 router.get("/",function (req, res){
     res.send("hey its working");
 });
+router.post("/register", async function (req, res){
+    let  { email, password, fullname } = req.body;
+
+   let user = await userModel.create({
+        email,
+        password,
+        fullname,
+    });
+});
+
 
 module.exports = router;
